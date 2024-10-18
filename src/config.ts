779b96fde4bfbd73c4769fb682269a0cdd80300b
Ayush Kapoor <stringakaak@gmail.com>
2024-10-18 13:44:31 +0530

    moved date and report folder vars to config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,3 +8,8 @@ export const ASANA_USER_ID = process.env.ASANA_USER_ID;
 export const ASANA_WORKSPACE_ID = process.env.ASANA_WORKSPACE_ID;
 export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+export const EOD_REPORT_FOLDER = "eod-report";
+export const EOD_DATE = dayjs("17 October 2024");
+
+export const POST_TO_ASANA = false;
